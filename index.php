3db372eb79e9057a265caa97e6e3cba7d193698a
Felipe Nascimento <felipejn@gmail.com>
2020-04-14 00:50:52 +0100

    Inicializando Git

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto E-Commmerce</title>
+</head>
+<body>
+	<h1><center>Projeto E-Commerce</center></h1>
+	<p>Início do projeto de website do curso de PHP 7 da Hcode.</p>
+</body>
+</html>
