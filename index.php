f330ff2837c7ad90dfc72fea42c9c32b51bc5230
Felipe Nascimento <felipejn@gmail.com>
2020-04-16 15:22:02 +0100

    Tela de Login

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -20,9 +22,39 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
 
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/login', function() {
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+});
+
+$app->post('/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /login");
+	exit;
 
 });
 
